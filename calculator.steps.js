60f835ecac5eb54c747bb3b0684db50177ce61e8
Lucia <112613922+luu522@users.noreply.github.com>
2022-09-02 14:09:15 +0200

    Add files via upload

---
+++ calculator.steps.js
@@ -0,0 +1,12 @@
+const {Given, Then} = require('@cucumber/cucumber');
+//const{expect} = require('@playwright/steps');
+
+Given('a user opens the app', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('in the display screen should show the following value:  {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
---
+++ minicalc.feature
@@ -0,0 +1,9 @@
+Feature: Calculator
+
+Background: 
+    Given a user opens the app
+    
+Scenario: Default display screen                                                                        
+Then in the display screen should show the following value:  "0"
+# And no button should be highlighted
+# And all buttons should be enabled except +/- and 0
